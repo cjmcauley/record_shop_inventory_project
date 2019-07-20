1b9c6d4a725efbfaa536ef3ed4bed671785259b0
cjmcauley <christopher.mcauley@gmail.com>
2019-07-21 00:45:10 +0100

    Edit page updated.

--- record_shop/controller.rb
+++ record_shop/controller.rb
@@ -6,25 +6,45 @@ require_relative './models/year'
 require_relative './models/record_label'
 also_reload './models/*'
 
+#INDEX
 get '/products' do
-  @products = Product.all
+  @products = Product.all()
   erb(:product)
 end
-
+#INDEX
 get '/record_labels' do
-  @record_labels = RecordLabel.all
+  @record_labels = RecordLabel.all()
   erb(:record_label)
 end
-
+#SHOW
+get '/products/:id' do
+  @products = Product.find( params[:id] )
+  erb( :show )
+end
+#CREATE
 post '/products' do
   @product = Product.new(params)
-  @product.save
+  @product.save()
   redirect '/products'
 end
-
+#NEW
 get '/products/new' do
-  @record_labels = RecordLabel.all
-  @years = Year.all
-  @formats = Format.all
-  erb(:new_product)
+  @record_labels = RecordLabel.all()
+  @years = Year.all()
+  @formats = Format.all()
+  erb(:new)
+ end
+#EDIT
+ get '/products/:id/edit' do
+   @record_labels = RecordLabel.all()
+   @years = Year.all()
+   @formats = Format.all()
+   @product = Product.find(params[:id])
+   erb(:edit)
+ end
+#UPDATE
+ post '/products/:id' do
+   product = Product.new(params)
+   product.update()
+   redirect '/products'
  end
--- record_shop/models/product.rb
+++ record_shop/models/product.rb
@@ -1,82 +1,96 @@
-require_relative '../db/sql_runner'
-require_relative 'format'
-require_relative 'year'
-require_relative 'record_label'
+  require_relative '../db/sql_runner'
+  require_relative 'format'
+  require_relative 'year'
+  require_relative 'record_label'
 
-class Product
-  attr_reader :id
-  attr_accessor :artist, :title, :quantity, :cost_price, :retail_price, :img_url, :record_label_id, :year_id, :format_id
+  class Product
+    attr_reader :id
+    attr_accessor :artist, :title, :quantity, :cost_price, :retail_price, :img_url, :record_label_id, :year_id, :format_id
 
-  def initialize(options)
-    @id = options['id'].to_i if options['id']
-    @artist = options['artist']
-    @title = options['title']
-    @quantity = options['quantity'].to_i
-    @cost_price = options['cost_price'].to_f
-    @retail_price = options['retail_price'].to_f
-    @img_url = options['img_url']
-    @record_label_id = options['record_label_id'].to_i
-    @year_id = options['year_id'].to_i
-    @format_id = options['format_id'].to_i
+    def initialize(options)
+      @id = options['id'].to_i if options['id']
+      @artist = options['artist']
+      @title = options['title']
+      @quantity = options['quantity'].to_i
+      @cost_price = options['cost_price'].to_f
+      @retail_price = options['retail_price'].to_f
+      @img_url = options['img_url']
+      @record_label_id = options['record_label_id'].to_i
+      @year_id = options['year_id'].to_i
+      @format_id = options['format_id'].to_i
+    end
+
+    def save
+      sql = '
+      INSERT INTO products
+      (artist, title, quantity, cost_price, retail_price, img_url, record_label_id, year_id, format_id)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+      RETURNING ID'
+      values = [@artist, @title, @quantity, @cost_price, @retail_price, @img_url, @record_label_id, @year_id, @format_id]
+      result = SqlRunner.run(sql, values)
+      @id = result.first['id'].to_i
+    end
+
+    def update()
+      sql = "UPDATE products
+      SET
+      (artist, title, quantity, cost_price, retail_price, img_url, record_label_id, year_id, format_id) =
+      ( $1, $2, $3, $4, $5, $6, $7, $8, $9 )
+      WHERE id = $10"
+      values = [@artist, @title, @quantity, @cost_price, @retail_price, @img_url, @record_label_id, @year_id, @format_id, @id]
+      SqlRunner.run( sql, values )
+    end
+
+    def record_label
+      sql = '
+      SELECT * FROM record_labels
+      WHERE id = $1'
+      results = SqlRunner.run(sql, [@record_label_id])
+      return RecordLabel.new(results.first)
+    end
+
+    def year
+      sql = '
+      SELECT * FROM years
+      WHERE id = $1'
+      results = SqlRunner.run(sql, [@year_id])
+      return Year.new(results.first)
+    end
+
+    def format
+      sql = '
+      SELECT * FROM formats
+      WHERE id = $1'
+      results = SqlRunner.run(sql, [@format_id])
+      return Format.new(results.first)
+    end
+
+    def self.find( id )
+      sql = 'SELECT * FROM products
+      WHERE id = $1'
+      values = [id]
+      product = SqlRunner.run( sql, values )
+      result = Product.new( product.first )
+      return result
+    end
+
+    def self.delete( id )
+      sql = "DELETE FROM products
+      WHERE id = $1"
+      values = [id]
+      SqlRunner.run( sql, values )
+    end
+
+    def self.delete_all
+      sql = 'DELETE FROM products'
+      SqlRunner.run(sql)
+    end
+
+    def self.all()
+      sql = "SELECT * FROM products"
+      products = SqlRunner.run( sql )
+      result = products.map { |product| Product.new( product ) }
+      return result
+    end
+
   end
-
-  def save
-    sql = '
-    INSERT INTO products
-    (artist, title, quantity, cost_price, retail_price, img_url, record_label_id, year_id, format_id)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-    RETURNING ID'
-    values = [@artist, @title, @quantity, @cost_price, @retail_price, @img_url, @record_label_id, @year_id, @format_id]
-    result = SqlRunner.run(sql, values)
-    @id = result.first['id'].to_i
-  end
-
-  def update()
-    sql = "UPDATE products
-    SET
-    (artist, title, quantity, cost_price, retail_price, img_url, record_label_id, year_id, format_id) =
-    ( $1, $2, $3, $4, $5, $6, $7, $8, $9 )
-    WHERE id = $10"
-    values = [@artist, @title, @quantity, @cost_price, @retail_price, @img_url, @record_label_id, @year_id, @format_id, @id]
-    SqlRunner.run( sql, values )
-  end
-
-  def record_label
-    sql = '
-    SELECT * FROM record_labels
-    WHERE id = $1'
-    results = SqlRunner.run(sql, [@record_label_id])
-    return RecordLabel.new(results.first)
-  end
-
-  def year
-    sql = '
-    SELECT * FROM years
-    WHERE id = $1'
-    results = SqlRunner.run(sql, [@year_id])
-    return Year.new(results.first)
-  end
-
-  def format
-    sql = '
-    SELECT * FROM formats
-    WHERE id = $1'
-    results = SqlRunner.run(sql, [@format_id])
-    return Format.new(results.first)
-  end
-
-
-
-  def self.delete( id )
-    sql = "DELETE FROM products
-    WHERE id = $1"
-    values = [id]
-    SqlRunner.run( sql, values )
-  end
-
-  def self.delete_all
-    sql = 'DELETE FROM products'
-    SqlRunner.run(sql)
-  end
-
-end
